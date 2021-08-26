940558ca20c61a5380a14c1002b0964f4d9d1152
Wesley Marinho <51543121+Wesley-Marinho@users.noreply.github.com>
2021-08-26 18:48:08 -0300

    Feito

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Header from './components/Header';
+import { CartProvider } from './hooks/useCart';
+import Routes from './routes/routes';
+import GlobalStyles from './styles/global';
+
+
+const App = (): JSX.Element => {
+  return (
+    <BrowserRouter>
+      <CartProvider>
+        <GlobalStyles />
+        <Header />
+        <Routes />
+        <ToastContainer autoClose={3000} />
+      </CartProvider>
+    </BrowserRouter>
+  );
+};
+
+export default App;
